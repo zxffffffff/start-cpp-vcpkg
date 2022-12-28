19632e8d1be2d9ece268deed2344fa2ed130dedc
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2022-12-28 16:46:45 +0800

    add glog

--- src/sample-tools/SampleTools.cpp
+++ src/sample-tools/SampleTools.cpp
@@ -1,7 +1,6 @@
 #include "SampleTools.h"
 #include "zlib.h"
-#include "uv.h"
-#include "curl/curl.h"
+#include "glog/logging.h"
 
 SampleTools::SampleTools()
 {
@@ -21,37 +20,12 @@ SampleTools::SampleTools()
         assert(strncmp((const char*)raw, (const char*)raw2, raw2_len) == 0);
     }
 
-    std::cout << "libuv version: " << uv_version_string() << std::endl;
+    std::cout << "glog" << std::endl;
     {
-        //static uv_loop_t loop;
-        //uv_tcp_t server;
-        //uv_tcp_init(&loop, &server);
-        //struct sockaddr_in addr;
-        //uv_ip4_addr("127.0.0.1", 12345, &addr);
-        //uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
-        //int ret = uv_listen((uv_stream_t*)&server, 128, [](uv_stream_t* server, int status) {
-        //    uv_tcp_t* client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-        //    uv_tcp_init(&loop, client);
-        //    if (uv_accept(server, (uv_stream_t*)client) == 0) {
-        //        //do_some_thind();
-        //    }
-        //});
-        //assert(ret == 0);
-    }
-
-    std::cout << "curl version: " << curl_version() << std::endl;
-    {
-        CURL* curl;
-        CURLcode res;
-        curl_global_init(CURL_GLOBAL_DEFAULT);
-        curl = curl_easy_init();
-        assert(curl);
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.baidu.com");
-        //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK)
-          fprintf(stderr, " curl_easy_perform () failed: %s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-        curl_global_cleanup();
+        assert(google::IsGoogleLoggingInitialized());
+        LOG(INFO) << "This is INFO";
+        LOG(WARNING) << "This is WARNING";
+        LOG(ERROR) << "This is ERROR";
+        //LOG(FATAL) << "This is FATAL";
     }
 }
