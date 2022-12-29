22891ec9f9d2b976d25cbaf721de8db6a52453d0
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2022-12-29 15:06:31 +0800

    改为 static lib

--- src/sample-datacenter/SampleDataCenter.h
+++ src/sample-datacenter/SampleDataCenter.h
@@ -1,18 +1,8 @@
 #pragma once
 
-#ifdef _WIN32
-# ifndef SAMPLE_DATACENTER_EXPORTS
-#  define SAMPLE_DATACENTER_API __declspec(dllexport)
-# else
-#  define SAMPLE_DATACENTER_API __declspec(dllimport)
-# endif
-#else
-# define SAMPLE_DATACENTER_API __attribute__((visibility("default")))
-#endif
-
 #include <iostream>
 
-class SAMPLE_DATACENTER_API SampleDataCenter
+class SampleDataCenter
 {
 public:
     SampleDataCenter();
--- src/sample-tools/SampleTools.h
+++ src/sample-tools/SampleTools.h
@@ -1,21 +1,11 @@
 #pragma once
 
-#ifdef _WIN32
-# ifndef SAMPLE_TOOLS_EXPORTS
-#  define SAMPLE_TOOLS_API __declspec(dllexport)
-# else
-#  define SAMPLE_TOOLS_API __declspec(dllimport)
-# endif
-#else
-# define SAMPLE_TOOLS_API __attribute__((visibility("default")))
-#endif
-
 #include <iostream>
 #include <string.h>
 #include <cassert>
 #include <thread>
 
-class SAMPLE_TOOLS_API SampleTools
+class SampleTools
 {
 public:
     SampleTools();
