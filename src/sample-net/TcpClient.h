2b904c5e0873ea9da289a7f429e0ab6cbc6773bf
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2022-12-30 18:02:51 +0800

    临时保存

---
+++ src/sample-net/TcpClient.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class TcpClient
+{
+public:
+    TcpClient();
+    ~TcpClient();
+};
---
+++ src/sample-net/TcpServer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class TcpServer
+{
+public:
+    TcpServer();
+    ~TcpServer();
+};
