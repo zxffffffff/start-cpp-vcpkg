db5f99c4fbe28c205f405028252e99481ac8ae72
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2023-02-23 14:09:48 +0800

    add mysql test

--- test/sample-db-test/mysql/MySQLClientTest.cpp
+++ test/sample-db-test/mysql/MySQLClientTest.cpp
@@ -29,6 +29,9 @@ TEST(MySQLClient, DDL) {
 
     MySQLClient client(host, port, user, pwd);
 
+    // 初始化
+    client.RunSQL("drop database test_db");
+
     // 创建数据库（schema）create database ...
     ASSERT_TRUE(client.RunSQL("create database test_db"));
 
@@ -58,6 +61,7 @@ TEST(MySQLClient, DML) {
     MySQLClient client(host, port, user, pwd);
 
     // 初始化
+    client.RunSQL("drop database test_db");
     ASSERT_TRUE(client.RunSQL("create database test_db"));
     ASSERT_TRUE(client.RunSQL("use test_db"));
     std::string sql = R"(create table if not exists `test_table`(
@@ -112,6 +116,12 @@ TEST(MySQLClient, DML) {
     //ASSERT_EQ(ret[2][2], "2023-02-21");
     //ASSERT_EQ(ret[3][2], "2023-02-21");
 
+    // 插入或替换（存在就删除） replace into ...
+    // 插入或更新（存在就更新） insert into ... on duplicate key update ...
+    // 插入或忽略（存在就忽略） insert ignore into ...
+    // todo ...
+
+
     // 删除数据库
     ASSERT_TRUE(client.RunSQL("drop database test_db"));
 }
@@ -144,6 +154,7 @@ TEST(MySQLClient, TCL) {
     MySQLClient client(host, port, user, pwd);
 
     // 初始化
+    client.RunSQL("drop database test_db");
     ASSERT_TRUE(client.RunSQL("create database test_db"));
     ASSERT_TRUE(client.RunSQL("use test_db"));
     std::string sql = R"(create table if not exists `test_table`(
@@ -159,77 +170,34 @@ TEST(MySQLClient, TCL) {
      * 原子性（Atomicity）   事务
      * 一致性（Consistency） binlog记录表结构变更；三种格式
      * 隔离性（Isolation）   事务四种隔离级别：    脏读  不可重读  幻读
-     *                       - Read Uncommitted      1       1       1      不建议
-     *                       - Read Committed                1       1      建议
-     *                       - Repeatable Read                       1      InnoDB 默认使用
-     *                       - Serializable                                 
+     *                       - read uncommitted      1       1       1      不建议
+     *                       - read committed                1       1      建议
+     *                       - repeatable read                       1      InnoDB 默认使用
+     *                       - serializable                                 
      * 持久性（Durability）  事务日志 redo log、undo log
      */
+    
+    // 开启一个事务 begin、start transaction
+    ASSERT_TRUE(client.RunSQL("begin;"));
+    
+    // 事物保存点，支持多个 savepoint 
+    // ...
 
-    std::thread t1([] {
-        MySQLClient client(host, port, user, pwd);
-        ASSERT_TRUE(client.RunSQL("use test_db"));
-
-        // 开启一个事务 BEGIN、START TRANSACTION
-        ASSERT_TRUE(client.RunSQL("begin;"));
-
-        // 事物保存点，支持多个 SAVEPOINT 
-        // ...
-
-        // 执行业务
-        std::stringstream ss;
-        ss << "insert into test_table (title,author,submission_date) values";
-        for (int i = 0; i < 1000; ++i) {
-            auto title = "\'title_" + std::to_string(i) + '\'';
-            auto author = i % 2 ? "\'author_A\'" : "\'author_B\'";
-            auto submission_date = "CURDATE()";
-            if (i > 0)
-                ss << ",";
-            ss << " (" << title << "," << author << "," << submission_date << ")";
-        }
-        ss << ";";
-        std::string sql = ss.str();
-        ASSERT_TRUE(client.RunSQL(sql));
-
-        // 提交事务 COMMIT 回滚事务 ROLLBACK
-        ASSERT_TRUE(client.RunSQL("commit;"));
-
-        // 隔离级别 SET TRANSACTION
-    });
-
-    std::thread t2([] {
-        MySQLClient client(host, port, user, pwd);
-        ASSERT_TRUE(client.RunSQL("use test_db"));
-
-        // read
-    });
-
-    t1.join();
-    t2.join();
-
-    // 删除数据库
-    ASSERT_TRUE(client.RunSQL("drop database test_db"));
-}
-
-TEST(MySQLClient, DeadTest) {
-    if (skip_test) return;
-
-    MySQLClient client(host, port, user, pwd);
-
-    // 初始化
-    ASSERT_TRUE(client.RunSQL("create database test_db"));
-    ASSERT_TRUE(client.RunSQL("use test_db"));
-    std::string sql = R"(create table if not exists `test_table`(
-            `id` int unsigned auto_increment,
-            `title` varchar(100) not null,
-            `author` varchar(40) not null,
-            `submission_date` date,
-            primary key ( `id` )
-        )engine=innodb default charset=utf8;)";
-    ASSERT_TRUE(client.RunSQL(sql));
-
-    // todo ...
-
+    // 默认开启自动提交，SET AUTOCOMMIT=0 禁止自动提交
+    
+    // 提交事务 commit
+    ASSERT_TRUE(client.RunSQL("commit;"));
+    // 回滚事务 rollback
+    ASSERT_TRUE(client.RunSQL("rollback;"));
+    
+    // 隔离级别 set session/global transaction isolation level ...
+    // 主要通过锁实现隔离，不同级别锁针对 update(脏读) 和 insert(幻读) 锁的粒度不同
+    ASSERT_TRUE(client.RunSQL("set session transaction isolation level read uncommitted;")); // 脏读
+    
+    // 两种 select 读锁
+    // lock in share mode   共享锁，可以多个会话读
+    // for update           排他锁，和 update 操作一样级别的锁，禁止其他 update
+    ASSERT_TRUE(client.RunSQL("select * from test_table where author='author_B' for update;"));
 
     // 删除数据库
     ASSERT_TRUE(client.RunSQL("drop database test_db"));
