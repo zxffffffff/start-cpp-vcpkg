f2a2c766023e3afbecc55c19112d338264928f47
Mark <132647029+1337328542@users.noreply.github.com>
2023-08-16 18:41:43 +0800

    cmakelist增加h

--- src/sample-tools/thread/impl/stl_threadtimer_impl.h
+++ src/sample-tools/thread/impl/stl_threadtimer_impl.h
@@ -15,8 +15,8 @@ class ThreadTimerImpl : public IThreadTimer
     struct ThreadData
     {
         std::function<void()> m_callback;
-        std::atomic_int m_interval_ms{ 50 };
-        std::atomic_bool m_running{ false };
+        std::atomic_int m_interval_ms{50};
+        std::atomic_bool m_running{false};
     };
     std::unique_ptr<ThreadData> thread_data = std::make_unique<ThreadData>();
 
@@ -29,7 +29,7 @@ public:
         SetInterval(ms);
         SetCbk(f);
     }
-    virtual ~ThreadTimerImpl() 
+    virtual ~ThreadTimerImpl()
     {
         Stop();
     }
@@ -39,12 +39,12 @@ public:
         thread_data->m_interval_ms = ms;
     }
 
-    virtual int  GetInterval() const override
+    virtual int GetInterval() const override
     {
         return thread_data->m_interval_ms;
     }
 
-    virtual void SetCbk(std::function<void()> f)override
+    virtual void SetCbk(std::function<void()> f) override
     {
         thread_data->m_callback = f;
     }
@@ -55,7 +55,7 @@ public:
             return;
         thread_data->m_running = true;
 
-        //LOG(INFO) << __FUNCTION__ << " interval=" << thread_data->m_interval_ms;
+        // LOG(INFO) << __FUNCTION__ << " interval=" << thread_data->m_interval_ms;
         m_thread = std::thread(&ThreadTimerImpl::run, thread_data.get());
     }
 
@@ -65,7 +65,7 @@ public:
         Start();
     }
 
-    virtual void Start(int ms, std::function<void()> f)override
+    virtual void Start(int ms, std::function<void()> f) override
     {
         SetInterval(ms);
         SetCbk(f);
@@ -78,7 +78,7 @@ public:
             return;
         thread_data->m_running = false;
 
-        //LOG(INFO) << __func__;
+        // LOG(INFO) << __func__;
         if (m_thread.joinable())
         {
             m_thread.join();
@@ -91,34 +91,37 @@ public:
     }
 
 private:
-    static void run(ThreadData* thread_data)
+    static void run(ThreadData *thread_data)
     {
+        using namespace std::chrono;
+        using namespace std::chrono_literals;
+
         size_t count = 0;
         while (thread_data->m_running)
         {
-            auto start = std::chrono::steady_clock::now();
-            std::chrono::milliseconds duration(thread_data->m_interval_ms);
-            while (thread_data->m_running && std::chrono::steady_clock::now() - start < duration)
+            auto start = steady_clock::now();
+            milliseconds duration(thread_data->m_interval_ms);
+            while (thread_data->m_running && duration_cast<milliseconds>(steady_clock::now() - start).count() < duration.count())
             {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                std::this_thread::sleep_for(10ms);
             }
 
             if (thread_data->m_running)
             {
                 if (thread_data->m_callback)
                 {
-                    //DLOG(INFO) << __func__
-                    //    << " interval=" << thread_data->m_interval_ms
-                    //    << " count=" << ++count;
+                    // DLOG(INFO) << __func__
+                    //     << " interval=" << thread_data->m_interval_ms
+                    //     << " count=" << ++count;
 
                     thread_data->m_callback();
                 }
                 else
                 {
-                    //DLOG(WARNING) << __func__
-                    //    << " interval=" << thread_data->m_interval_ms
-                    //    << " count=" << ++count
-                    //    << " no callback !";
+                    // DLOG(WARNING) << __func__
+                    //     << " interval=" << thread_data->m_interval_ms
+                    //     << " count=" << ++count
+                    //     << " no callback !";
                 }
             }
         }
