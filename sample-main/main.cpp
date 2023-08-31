2f544123c4989a401381abcf22544bf3d2ba3c17
Mark <132647029+1337328542@users.noreply.github.com>
2023-08-31 11:18:15 +0800

    win32设置命令行utf-8

--- sample-main/main.cpp
+++ sample-main/main.cpp
@@ -5,9 +5,13 @@
 ** Support	: zxffffffff@outlook.com, 1337328542@qq.com
 **
 ****************************************************************************/
-#include "SampleDynamicLib.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
+#include "SampleDynamicLib.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif // _WIN32
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
 /* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
@@ -17,18 +21,14 @@
 #pragma warning(disable:4566)
 #endif
 
-//#ifdef _WIN32
-//#include <Windows.h>
-//#endif // _WIN32
-
 int main(int argc, char *argv[])
 {
-//#ifdef _WIN32
-//    /* 命令行兼容utf-8 */
-//    SetConsoleOutputCP(CP_UTF8);
-//#endif // _WIN32
+#ifdef _WIN32
+    /* 命令行统一 utf-8 */
+    SetConsoleOutputCP(CP_UTF8);
+#endif // _WIN32
 
-    gflags::ParseCommandLineFlags(&argc, &argv, true); 
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
 
     std::cout << "你好 SampleMain." << std::endl;
