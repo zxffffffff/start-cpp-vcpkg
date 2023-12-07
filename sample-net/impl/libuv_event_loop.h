f6ff8e50b54bf91edbc662121b1fac045545333f
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2023-12-07 18:03:31 +0800

    兼容Linux

--- sample-net/impl/libuv_event_loop.h
+++ sample-net/impl/libuv_event_loop.h
@@ -1,3 +1,10 @@
+/****************************************************************************
+** MIT License
+**
+** Author	: xiaofeng.zhu
+** Support	: zxffffffff@outlook.com, 1337328542@qq.com
+**
+****************************************************************************/
 #pragma once
 #include "../interface/tcp_interface.h"
 #include <uv.h>
