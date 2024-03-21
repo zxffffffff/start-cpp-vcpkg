1bfb778dcd103284700158213d761bb21de5a7bf
xiaofeng.zhu <34299738+zxffffffff@users.noreply.github.com>
2024-03-21 15:48:15 +0800

    TODO imgui

--- sample-main-imgui/main.cpp
+++ sample-main-imgui/main.cpp
@@ -5,7 +5,12 @@
 ** Support	: zxffffffff@outlook.com, 1337328542@qq.com
 **
 ****************************************************************************/
+// [0]
+#include <GLFW/glfw3.h>
+// [1]
 #include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
 /* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
@@ -15,7 +20,69 @@
 #pragma warning(disable : 4566)
 #endif
 
+void loop()
+{
+}
+
 int main(int, char *[])
 {
+    // [0]
+    if (!glfwInit())
+    {
+        // Initialization failed
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "GLFWwindow", NULL, NULL);
+    if (!window)
+    {
+        // Window or OpenGL context creation failed
+        return -2;
+    }
+    // glfwMakeContextCurrent(window);
+
+    // [1]
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+
+    // [0]
+    while (!glfwWindowShouldClose(window))
+    {
+        // [1]
+        // (Your code calls glfwPollEvents())
+        // ...
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow(); // Show demo window! :)
+
+        loop();
+
+        // [1]
+        // Rendering
+        // (Your code clears your framebuffer, renders your other stuff etc.)
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        // (Your code calls glfwSwapBuffers() etc.)
+    }
+
+    // [1]
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    // [0]
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
