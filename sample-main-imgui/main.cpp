4360eeb5d2f6ef91863d3464a4a35b1d614d7b7d
xiaofeng.zhu <34299738+zxffffffff@users.noreply.github.com>
2024-03-21 10:44:27 +0800

    引入Dear ImGui

---
+++ sample-main-imgui/main.cpp
@@ -0,0 +1,21 @@
+/****************************************************************************
+** MIT License
+**
+** Author	: xiaofeng.zhu
+** Support	: zxffffffff@outlook.com, 1337328542@qq.com
+**
+****************************************************************************/
+#include "imgui.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
+/* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
+#if (_MSC_VER >= 1700)
+#pragma execution_character_set("utf-8")
+#endif
+#pragma warning(disable : 4566)
+#endif
+
+int main(int, char *[])
+{
+    return 0;
+}
