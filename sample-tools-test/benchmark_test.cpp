5c50d9fa538492db99aec2bf4aecf13ba7883072
xiaofeng.zhu <34299738+zxffffffff@users.noreply.github.com>
2024-08-13 15:38:03 +0800

    新增性能测试工具

---
+++ sample-tools-test/benchmark_test.cpp
@@ -0,0 +1,42 @@
+/****************************************************************************
+** MIT License
+**
+** Author	: xiaofeng.zhu
+** Support	: zxffffffff@outlook.com, 1337328542@qq.com
+**
+****************************************************************************/
+#include <gtest/gtest.h>
+#include "benchmark.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
+/* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
+#if (_MSC_VER >= 1700)
+#pragma execution_character_set("utf-8")
+#endif
+#pragma warning(disable : 4566)
+#endif
+
+TEST(benchmark, once)
+{
+    for (int i = 0; i < Hardware::GetCPUs(); ++i)
+    {
+        double use_ms = Benchmark::RunOnce();
+        double score = Benchmark::Score(use_ms);
+
+        std::cout << "benchmark score: " << score << std::endl;
+        if (use_ms < 100)
+            ASSERT_GT(score, 0);
+        else if (use_ms > 100)
+            ASSERT_LT(score, 0);
+        else /* 100 */
+            ASSERT_EQ(score, 0);
+    }
+}
+
+TEST(benchmark, multi_thread)
+{
+    double score = Benchmark::RunMultiThread(3);
+
+    std::cout << "benchmark multi_thread score: " << score << std::endl;
+    ASSERT_NE(score, 0);
+}
